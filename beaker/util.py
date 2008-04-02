ad45a77d199c46ddedf5d1aa54780b95d4bd3279
Ben Bangert <ben@groovie.org>
2008-04-02 14:40:28 -0700

    * SECURITY BUG: Fixed security issue with Beaker not properly removing   directory escaping characters from the session ID when un-signed sessions   are used. Reported with patch by Felix Schwarz.
    --HG--
    branch : trunk

--- beaker/util.py
+++ beaker/util.py
@@ -172,6 +172,7 @@ def encoded_path(root, identifiers, extension = ".enc", depth = 3, digest = True
 
     if digest:
         ident = sha.new(ident).hexdigest()
+        ident = os.path.basename(ident)
 
     tokens = []
     for d in range(1, depth):
