57dc9ab7f90923b28257b90b84c12e889c2334e2
Jon M. Dugan <jdugan@x1024.net>
2009-03-30 14:47:57 -0700

    add CacheManager.cache() decorator to create a cache without using a region add tests for for CacheManager.cache()
    --HG--
    branch : trunk

--- beaker/cache.py
+++ beaker/cache.py
@@ -70,9 +70,25 @@ except (InvalidCacheBackendError, SyntaxError), e:
 
 
 class Cache(object):
-    """Front-end to the containment API implementing a data cache."""
+    """Front-end to the containment API implementing a data cache.
+
+    ``namespace``
+        the namespace of this Cache
+
+    ``type``
+        type of cache to use
+
+    ``expire``
+        seconds to keep cached data
+
+    ``expiretime``
+        seconds to keep cached data (legacy support)
+
+    ``starttime``
+        time when cache was cache was
+    """
     def __init__(self, namespace, type='memory', expiretime=None,
-                 starttime=None, **nsargs):
+                 starttime=None, expire=None, **nsargs):
         try:
             cls = clsmap[type]
             if isinstance(cls, InvalidCacheBackendError):
@@ -81,7 +97,7 @@ class Cache(object):
             raise TypeError("Unknown cache implementation %r" % type)
             
         self.namespace = cls(namespace, **nsargs)
-        self.expiretime = expiretime
+        self.expiretime = expiretime or expire
         self.starttime = starttime
         self.nsargs = nsargs
         
@@ -207,7 +223,33 @@ class CacheManager(object):
                 def go():
                     return func(*args)
                 
-                return cache[0].get_value(cache_key, createfunc=go,
-                                          expiretime=reg['expire'])
+                return cache[0].get_value(cache_key, createfunc=go)
+            return cached
+        return decorate
+
+    def cache(self, *args, **kwargs):
+        """Decorate a function to cache itself with supplied parameters
+
+        ``args`` 
+            used to make the key unique for this function, as in region()
+            above.
+
+        ``kwargs``
+            parameters to be passed to get_cache(), will override defaults
+
+        """
+        cache = [None]
+        key = " ".join(str(x) for x in args)
+        
+        def decorate(func):
+            def cached(*args):
+                if not cache[0]:
+                    namespace = util.func_namespace(func)
+                    cache[0] = self.get_cache(namespace, **kwargs)
+                cache_key = key + " " + " ".join(str(x) for x in args)
+                def go():
+                    return func(*args)
+
+                return cache[0].get_value(cache_key, createfunc=go)
             return cached
         return decorate
