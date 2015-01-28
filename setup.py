1421181fffa215069f9dd5aa02066728596a07e5
Alessandro Molina <alessandro.molina@axant.it>
2015-01-28 12:34:16 +0100

    Merge README.rst and setup.py description

--- setup.py
+++ setup.py
@@ -4,9 +4,15 @@ import re
 
 from setuptools import setup, find_packages
 
-v = open(os.path.join(os.path.dirname(__file__), 'beaker', '__init__.py'))
+here = os.path.abspath(os.path.dirname(__file__))
+v = open(os.path.join(here, 'beaker', '__init__.py'))
 VERSION = re.compile(r".*__version__ = '(.*?)'", re.S).match(v.read()).group(1)
 v.close()
+
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+except IOError:
+    README = ''
 
 extra = {}
 tests_require = ['nose', 'webtest', 'Mock']
@@ -28,60 +34,7 @@ if not sys.platform.startswith('java') and not sys.platform == 'cli':
 setup(name='Beaker',
       version=VERSION,
       description="A Session and Caching library with WSGI Middleware",
-      long_description="""\
-Cache and Session Library
-+++++++++++++++++++++++++
-
-About
-=====
-
-Beaker is a web session and general caching library that includes WSGI
-middleware for use in web applications.
-
-As a general caching library, Beaker can handle storing for various times
-any Python object that can be pickled with optional back-ends on a
-fine-grained basis.
-
-Beaker was built largely on the code from MyghtyUtils, then refactored and
-extended with database support.
-
-Beaker includes Cache and Session WSGI middleware to ease integration with
-WSGI capable frameworks, and is automatically used by `Pylons
-<http://pylonshq.com/>`_.
-
-
-Features
-========
-
-* Fast, robust performance
-* Multiple reader/single writer lock system to avoid duplicate simultaneous
-  cache creation
-* Cache back-ends include dbm, file, memory, memcached, and database (Using
-  SQLAlchemy for multiple-db vendor support)
-* Signed cookie's to prevent session hijacking/spoofing
-* Cookie-only sessions to remove the need for a db or file backend (ideal
-  for clustered systems)
-* Extensible Container object to support new back-ends
-* Cache's can be divided into namespaces (to represent templates, objects,
-  etc.) then keyed for different copies
-* Create functions for automatic call-backs to create new cache copies after
-  expiration
-* Fine-grained toggling of back-ends, keys, and expiration per Cache object
-
-
-Documentation
-=============
-
-Documentation can be found on the `Official Beaker Docs site
-<http://beaker.groovie.org/>`_.
-
-
-Source
-======
-
-The latest developer version is available in a `github repository
-<https://github.com/bbangert/beaker>`_.
-""",
+      long_description=README,
       classifiers=[
       'Development Status :: 5 - Production/Stable',
       'Environment :: Web Environment',
