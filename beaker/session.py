4594e25226e52a8e33f6c4cd11a281c7638c9af6
Kacper Krupa <pagenoare@gmail.com>
2018-04-25 18:13:49 +0200

    moving code around to put it after check for timeout & save_accessed_time

--- beaker/session.py
+++ beaker/session.py
@@ -148,13 +148,6 @@ class Session(dict):
         self.namespace_class = namespace_class or clsmap[self.type]
 
         self.namespace_args = namespace_args
-        # We want to pass timeout param to redis backend to support expiration of keys
-        # In future, I believe, we can use this param for memcached and mongo as well
-        if timeout is not None and self.type == 'ext:redis':
-            # The backend expiration should always be a bit longer (I decied to use 2 minutes) than the
-            # session expiration itself to prevent the case where the backend data expires while
-            # the session is being read (PR#153)
-            self.namespace_args['timeout'] = timeout + 60 * 2
 
         self.request = request
         self.data_dir = data_dir
@@ -163,6 +156,14 @@ class Session(dict):
         if timeout and not save_accessed_time:
             raise BeakerException("timeout requires save_accessed_time")
         self.timeout = timeout
+        # We want to pass timeout param to redis backend to support expiration of keys
+        # In future, I believe, we can use this param for memcached and mongo as well
+        if self.timeout is not None and self.type == 'ext:redis':
+            # The backend expiration should always be a bit longer (I decied to use 2 minutes) than the
+            # session expiration itself to prevent the case where the backend data expires while
+            # the session is being read (PR#153)
+            self.namespace_args['timeout'] = self.timeout + 60 * 2
+
         self.save_atime = save_accessed_time
         self.use_cookies = use_cookies
         self.cookie_expires = cookie_expires
