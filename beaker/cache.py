96283fe89240040bf979e8555ba73571876e837c
Wolfgang Schnerring <wosc@wosc.de>
2025-12-03 19:25:50 +0100

    Load entry points with importlib.metadata instead of deprecated pkg_resources (#249)
    * Select exception class via `except Type:` instead of manual `isinstance`
    
    * Load entry points with importlib.metadata instead of deprecated pkg_resources
    
    I'm not sure in which circumstance DistributionNotFound may have been
    raised here previously, why it has been desireable to suppress that,
    and what the current machinery would raise instead, but if in doubt
    I'd rather have a warning than things silently not working.
    
    * Remove pkg_resources import guard, importlib is part of the stdlib, so always present
    
    * Remove long-obsolete python-2 compatibility code
    
    It especially makes no sense now anymore, since importlib is python-3 only.
    
    * Use importlib.metatdata API that's backward-compatible with Python<3.10

--- beaker/cache.py
+++ beaker/cache.py
@@ -6,7 +6,11 @@ as well as the function decorators :func:`.region_decorate`,
 :func:`.region_invalidate`.
 
 """
+import importlib.metadata
+import sys
+import traceback
 import warnings
+from io import StringIO
 from itertools import chain
 
 from beaker._compat import u_, unicode_text, func_signature, bindfuncargs
@@ -80,42 +84,32 @@ class _backends(object):
             raise e
 
     def _init(self):
-        try:
-            import pkg_resources
-
-            # Load up the additional entry point defined backends
-            for entry_point in pkg_resources.iter_entry_points('beaker.backends'):
-                try:
-                    namespace_manager = entry_point.load()
-                    name = entry_point.name
-                    if name in self._clsmap:
-                        raise BeakerException("NamespaceManager name conflict,'%s' "
-                                              "already loaded" % name)
-                    self._clsmap[name] = namespace_manager
-                except (InvalidCacheBackendError, SyntaxError):
-                    # Ignore invalid backends
-                    pass
-                except:
-                    import sys
-                    from pkg_resources import DistributionNotFound
-                    # Warn when there's a problem loading a NamespaceManager
-                    if not isinstance(sys.exc_info()[1], DistributionNotFound):
-                        import traceback
-                        try:
-                            from StringIO import StringIO  # Python2
-                        except ImportError:
-                            from io import StringIO        # Python3
-
-                        tb = StringIO()
-                        traceback.print_exc(file=tb)
-                        warnings.warn(
-                            "Unable to load NamespaceManager "
-                            "entry point: '%s': %s" % (
-                                        entry_point,
-                                        tb.getvalue()),
-                                        RuntimeWarning, 2)
-        except ImportError:
-            pass
+        # Load up the additional entry point defined backends
+        if sys.version_info < (3, 10):
+            entry_points = importlib.metadata.entry_points()['beaker.backends']
+        else:
+            entry_points = importlib.metadata.entry_points(group='beaker.backends')
+        for entry_point in entry_points:
+            try:
+                namespace_manager = entry_point.load()
+                name = entry_point.name
+                if name in self._clsmap:
+                    raise BeakerException("NamespaceManager name conflict,'%s' "
+                                          "already loaded" % name)
+                self._clsmap[name] = namespace_manager
+            except (InvalidCacheBackendError, SyntaxError):
+                # Ignore invalid backends
+                pass
+            except Exception:
+                # Warn when there's a problem loading a NamespaceManager
+                tb = StringIO()
+                traceback.print_exc(file=tb)
+                warnings.warn(
+                    "Unable to load NamespaceManager "
+                    "entry point: '%s': %s" % (
+                                entry_point,
+                                tb.getvalue()),
+                                RuntimeWarning, 2)
 
 # Initialize the basic available backends
 clsmap = _backends({
